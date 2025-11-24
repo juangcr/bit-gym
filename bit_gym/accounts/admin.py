33714e7f3eae5433f1b78984df523ea578d0434b
Juan G. Cortés <juang.cortes@outlook.com>
2025-11-23 23:12:48 -0600

    CustomUsers creation.

--- bit_gym/accounts/admin.py
+++ bit_gym/accounts/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+
+class CustomUserAdmin(UserAdmin):
+    """Users lists. Add groups separately (Admin, Staff, Customers)."""
+
+    model = CustomUser
+    list_display = (
+        "customer_id",
+        "username",
+        "email",
+        "is_basic",
+        "is_premium",
+        "is_staff",
+        "is_superuser",
+    )
+    list_filter = (
+        "is_basic",
+        "is_premium",
+        "is_staff",
+        "is_superuser",
+    )
+    search_fields = (
+        "username",
+        "email",
+    )
+    ordering = ("username",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- bit_gym/accounts/models.py
+++ bit_gym/accounts/models.py
@@ -1,3 +1,12 @@
+import uuid
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    is_basic = models.BooleanField(default=False)
+    is_premium = models.BooleanField(default=False)
+    customer_id = models.UUIDField(default=uuid.uuid4, editable=False)
+
+    def __str__(self):
+        return self.username
